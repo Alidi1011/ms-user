9ac022b13d6aa50521e7abba947156e19a0618b0
alisson <arteaga10alisson@gmail.com>
2024-10-26 00:08:18 -0500

    first commit ms-user

---
+++ src/test/java/com/aarteaga/ms_user/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarteaga.ms_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
